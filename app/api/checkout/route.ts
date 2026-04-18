57fa5a39a80ea34f3423c93877fe77d47ba1d08d
zeth30 <kfasselt.schule@gmail.com>
2026-04-18 13:57:43 +0200

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -15,7 +15,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2023-10-16",
 });
 
 export async function POST(req: NextRequest) {
