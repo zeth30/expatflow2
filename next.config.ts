a896341b6004cea0e285c6f49bafc3c3f359f59a
zeth30 <kfasselt.schule@gmail.com>
2026-04-18 19:05:51 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      root: "/Users/zeth/my-berlin-butler",
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
